457365c6767bdf05bc21363dea7192c67762f6f7
Ishola Janet <janetishola2010@gmail.com>
2022-02-17 16:11:57 +0100

    update

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,3 +1,4 @@
+import { Schema } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 
 
@@ -6,7 +7,8 @@ export const UserSchema = new mongoose.Schema({
     lastname: String,
     phonenumber: String,
     email: String,
+    account: { type: mongoose.Schema.Types.ObjectId, ref: 'Account' }
 
-    
-
-}, {timestamps: { createdAt: 'created_at', updatedAt: 'updated_at'}});
+},
+{timestamps: { createdAt: 'created_at', updatedAt: 'updated_at'}
+});
